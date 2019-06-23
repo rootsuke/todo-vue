a6b80a5cab3c02f5bb111f6f9c98d955d53a9efd
root_suke <rootsuke@gmail.com>
2019-06-23 19:25:16 +0900

    add function to return json file

--- app/controllers/api/tasks_controller.rb
+++ app/controllers/api/tasks_controller.rb
@@ -1,2 +1,29 @@
 class Api::TasksController < ApplicationController
+  def index
+    @tasks = Task.order(updated_at: :desc)
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      render :show, status: :created
+    else
+      render json: @task.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if task.update_attributes(task_params)
+      render :show, status: :ok
+    else
+      render json: @task.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+    def task_params
+      params.fetch(:task, {}).permit(:name, :done)
+    end
 end
